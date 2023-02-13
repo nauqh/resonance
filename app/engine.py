c6d979bb0c3249bb6a6892725de2c9089961f654
nauqh <hodominhquan.work@gmail.com>
2023-02-13 19:02:41 +0700

    change engine

--- app/engine.py
+++ app/engine.py
@@ -1,68 +1,46 @@
-import re
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics.pairwise import cosine_similarity
 
 
 class Pipeline():
-    def __init__(self) -> None:
+    def __init__(self):
         pass
 
     def select_cols(self, df):
         return df[['track_id', 'danceability', 'energy', 'key', 'loudness', 'mode',
-                   'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'track_pop', 'time_signature']]
+                   'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'track_pop']]
 
     def ohe(self, df: pd.DataFrame, column: str, new_name: str):
-        """
-        Create One Hot Encoded features of a specific column
-
-        Args: 
-            df: Spotify Dataframe
-            column: Column to be processed
-            new_name: new column name to be used
-
-        Return: 
-            tf_df: One-hot encoded features 
-        """
-
         tf_df = pd.get_dummies(df[column])
         tf_df.columns = [new_name + "|" + str(i) for i in tf_df.columns]
         tf_df.reset_index(drop=True, inplace=True)
         return tf_df
 
     def create_feature_set(self, df, float_cols) -> pd.DataFrame:
-        """
-        Process spotify df to create a final set of features that will be used to generate recommendations
-
-        Args: 
-            df: Spotify Dataframe
-            float_cols: List of float columns that will be scaled
-
-        Returns: 
-            Final set of features 
-        """
+        scaler = StandardScaler()
 
         # One-hot Encoding
-        key_ohe = self.ohe(df, 'key', 'key') * 0.5
-        mode_ohe = self.ohe(df, 'mode', 'mode') * 0.5
-        time_ohe = self.ohe(df, 'time_signature', 'time_signature') * 0.5
+        key_ohe = self.ohe(df, 'key', 'key')
+        key_ohe = pd.DataFrame(scaler.fit_transform(
+            key_ohe), columns=key_ohe.columns)
+        mode_ohe = self.ohe(df, 'mode', 'mode')
+        mode_ohe = pd.DataFrame(scaler.fit_transform(
+            mode_ohe), columns=mode_ohe.columns)
 
-        # Normalization
         # Scale popularity columns
         pop = df[["track_pop"]].reset_index(drop=True)
-        scaler = MinMaxScaler()
         pop_scaled = pd.DataFrame(
-            scaler.fit_transform(pop), columns=pop.columns) * 0.2
+            scaler.fit_transform(pop), columns=pop.columns)
 
         # Scale audio columns
         floats = df[float_cols].reset_index(drop=True)
-        scaler = MinMaxScaler()
-        floats_scaled = pd.DataFrame(scaler.fit_transform(
-            floats), columns=floats.columns) * 0.2
+        floats_scaled = pd.DataFrame(
+            scaler.fit_transform(floats), columns=floats.columns)
 
         # Concanenate all features
         final = pd.concat([floats_scaled, pop_scaled,
-                          key_ohe, mode_ohe, time_ohe], axis=1)
+                          key_ohe, mode_ohe], axis=1)
 
         # Add song id
         final['id'] = df['track_id'].values
@@ -72,14 +50,7 @@ class Pipeline():
     def create_playlist_vector(self, playlist: pd.DataFrame) -> list:
         """
         Summarize user's playlist into a single vector
-
-        Args:
-            playlist: dataset of song playlist
-
-        Returns:
-            Dataframe of playlist audio features
         """
-
         playlist = playlist.drop(columns="id")
 
         return playlist.sum(axis=0)
@@ -126,7 +97,7 @@ class Pipeline():
 
 
 def recommend(playlist: pd.DataFrame):
-    features = pd.read_csv("data/features.csv")
+    features = pd.read_csv("features.csv")
     float_cols = playlist.dtypes[playlist.dtypes == 'float64'].index.values
 
     pl = Pipeline()
