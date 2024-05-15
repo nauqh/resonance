12c3e7e7acf29fa73dabb36322caa21606fadc1f
Nauqh <hodominhquan.work@gmail.com>
2024-05-16 02:10:54 +1000

    fix(env): add env config

--- backend/tests/test_engine.py
+++ backend/tests/test_engine.py
@@ -8,42 +8,43 @@ from src.utils.utils import (
 import json
 
 
-# def test_search_artist():
-#     """
-#     Search artist given name
-#     """
-#     artist = search_artist("Justin Bieber")
-#     assert artist['items'][0]['name'] == "Justin Bieber"
+def test_search_artist():
+    """
+    Search artist given name
+    """
+    artist = search_artist("Justin Bieber")
+    assert artist['items'][0]['name'] == "Justin Bieber"
 
-#     artist = search_artist("Hà Anh Tuấn")
-#     print(json.dumps(artist, indent=2))
-#     assert artist['items'][0]['name'] == "Hà Anh Tuấn"
-
-
-# def test_get_artist():
-#     """
-#     Get artist given artist id
-#     """
-#     artist = get_artist('1uNFoZAHBGtllmzznpCI3s')
-#     print(json.dumps(artist, indent=2))
-#     assert artist['name'] == "Justin Bieber"
+    artist = search_artist("Hà Anh Tuấn")
+    print(json.dumps(artist, indent=2))
+    assert artist['items'][0]['name'] == "Hà Anh Tuấn"
 
 
-# def test_get_playlist():
-#     """
-#     Get playlist given url
-#     """
-#     playlist = get_playlist(
-#         "https://open.spotify.com/playlist/2xukpbxolEK8C9HdpANzZu?si=bbe1304cc2f74b31")
-#     assert playlist['type'] == 'playlist'
+def test_get_artist():
+    """
+    Get artist given artist id
+    """
+    artist = get_artist('1uNFoZAHBGtllmzznpCI3s')
+    print(json.dumps(artist, indent=2))
+    assert artist['name'] == "Justin Bieber"
 
 
-# def test_search_playlist():
-#     """
-#     Search playlist with keyword
-#     """
-#     playlist = search_playlist("korean soft indie")
-#     assert "korean" in playlist['name']
+def test_get_playlist():
+    """
+    Get playlist given url
+    """
+    playlist = get_playlist(
+        "https://open.spotify.com/playlist/2xukpbxolEK8C9HdpANzZu?si=bbe1304cc2f74b31")
+    assert playlist['type'] == 'playlist'
+
+
+def test_search_playlist():
+    """
+    Search playlist with keyword
+    """
+    playlist = search_playlist("korean soft indie")
+    print(json.dumps(playlist, indent=2))
+    assert "korean" in playlist['name']
 
 
 def test_get_recommendation():
